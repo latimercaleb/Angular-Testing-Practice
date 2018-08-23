34b9833a69ce43eb25f099be941089dd6aa4a6b3
latimeks <clatimer@revspringinc.com>
2018-08-23 13:28:11 -0400

    First integration test complete, component + template

--- src/app/Integration Practice/1-voter/voter.component.spec.ts
+++ src/app/Integration Practice/1-voter/voter.component.spec.ts
@@ -1,10 +1,30 @@
 import { VoterComponent } from './voter.component';
-
-describe('VoterComponent', () => {
-
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import{By} from '@angular/platform-browser'
+fdescribe('VoterComponent', () => {
+  let component :VoterComponent;
+  let fixture :ComponentFixture<VoterComponent>;
   beforeEach(() => {
+    // In integration tests, Angular must make an instance of the component, most of this is done automagically from the cli
+    TestBed.configureTestingModule({
+    declarations:[VoterComponent]
+    });
+    fixture = TestBed.createComponent(VoterComponent); // Wrapper around component instance, gives you access to both the class + template
+    component = fixture.componentInstance; // Extract the base component
   });
 
-  it('', () => {
+  it('should render total votes in template', () => {
+    component.othersVote = 10;
+    component.myVote = 1;
+    fixture.detectChanges();
+    let templateTag = fixture.debugElement.query(By.css('.vote-count'));
+
+    expect(templateTag.nativeElement.innerText).toContain(11);
+  });
+  it('should highlight upvote button if previously upvoted',() => {
+    component.myVote = 1;
+    fixture.detectChanges();
+    let templateTag = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(templateTag.classes['highlighted']).toBeTruthy();
   });
 });
--- src/app/extra-practice/4- user-component/users.component.spec.ts
+++ src/app/extra-practice/4- user-component/users.component.spec.ts
@@ -5,7 +5,7 @@
 import {UsersComponent} from './users.component';
 import {UserService} from './user.service';
 import {Observable,of,throwError} from 'rxjs';
-fdescribe('Exercise 4: Compnents & Services',() => {
+describe('Exercise 4: Compnents & Services',() => {
   describe('Users Component should, ',() => {
     let service: UserService;
     let users: UsersComponent;
@@ -21,23 +21,6 @@ fdescribe('Exercise 4: Compnents & Services',() => {
       users.ngOnInit();
       expect(users.users).toEqual(ex);
     })
-    it('delete when confirmed', function(){
-      let simpleObj = {
-           "id": 1,
-           "name": "test"
-     };
-      users.users.push(simpleObj);
-      spyOn(window,'confirm').and.returnValue(true);
-      let spy = spyOn(service,'deleteUser').and.returnValue(of());
-      users.deleteUser(simpleObj);
-      expect(spy).toHaveBeenCalledWith(simpleObj);
-    })
 
-    it('not delete if not confirmed', function(){
-      spyOn(window,'confirm').and.returnValue(false);
-      let spy = spyOn(service,'deleteUser').and.returnValue(of());
-      users.deleteUser(102);
-      expect(spy).toHaveBeenCalledWith(102);
-    })
   })
 })
